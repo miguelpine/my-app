8645335edcae35d178dfc2081fe14a4ecf583a86
Miguel P. Rodrigues <miguel.pinr@gmail.com>
2020-06-07 17:45:37 +0100

    :sunrise:

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Router } from './modules/router';
+import './App.css';
+
+export const App = () => {
+  return (
+    <div className="App">
+      {/* <header className="App-header" /> */}
+      <Router />
+    </div>
+  );
+}
+
---
+++ src/modules/router.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+export const Router = () => {
+  return (
+    <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            "Mike"
+          </Route>
+          <Route path="/about">
+            "Mafalda"
+          </Route>
+          <Route path="/dashboard">
+            "Cenas"
+          </Route>
+          <Route >
+              "404"
+          </Route>
+        </Switch>
+    </BrowserRouter>
+  );
+}
